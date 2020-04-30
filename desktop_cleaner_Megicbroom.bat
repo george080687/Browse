70197d228011ec7bbf01df74b8a27fa69f467be2
George <64363440+george080687@users.noreply.github.com>
2020-04-30 13:04:59 +0300

    Create desktop_cleaner_Megicbroom.bat

---
+++ desktop_cleaner_Megicbroom.bat
@@ -0,0 +1,29 @@
+md docx;
+move *.docx docx;
+md pptx;
+move *.pptx pptx;
+md txt;
+move *.txt txt;
+md jpg;
+move *.jpg jpg;
+md rar;
+move *.rar rar;
+md exe;
+move *.exe exe;
+md mp4;
+move *.mp4 mp4;
+md pdf;
+move *.pdf pdf;
+md url;
+move *.url url;
+md lnk;
+move *.lnk lnk;
+md doc;
+move *.doc doc;
+md rtf;
+move *.rtf rtf;
+md xls;
+move *.xls xls;
+//script file bat file for Windows
+start Megicbroom.bat
+execution team
